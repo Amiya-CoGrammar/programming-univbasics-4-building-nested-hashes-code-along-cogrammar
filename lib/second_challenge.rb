4721511765c35f2ecb281e31e37c685d0d068c50
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-08 09:28:38 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,30 +2,30 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {
-        
+
       }
       :matriarch=> {
-        
+
       }
       :hero => {
-        
+
       }
       :hero_friends = {
-        
+
       }
     },
     :capulet => {
       :patriarch => {
-        
+
       }
       :matriarch => {
-        
+
       }
       :heroine => {
-        
+
       }
       :heroine_friends = {
-        
+
       }
     }
   }
