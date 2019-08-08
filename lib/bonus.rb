06e32c4203841df5c307b4e64b8a5b7e320e18c7
CoGrammar Code Review <reviews@cogrammar.com>
2019-08-08 09:58:48 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,8 +22,13 @@ def bonus
 
   #code your solution here:
 
-  epic_tragedy[:montague][:hero][:status] = "dead"
-  epic_tragedy[:capulet][:heroine][:status] = "dead"
+  while epic_tragedy
+    epic_tragedy[:montague][:hero][:status] = "dead"
+  end
+
+  while epic_tragedy
+    epic_tragedy[:capulet][:heroine][:status] = "dead"
+  end
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
